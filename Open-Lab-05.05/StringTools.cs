eedeb0842a23867346df6b2da9581105b1d49033
Juraj Kultán <filipenkodavid@gmail.com>
2021-11-19 09:25:57 +0100

    I implemethed method that makes every second letter to Upper

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -6,7 +6,31 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            string newString = "";
+            int nexPos = 0;
+            for (int i = 0; i < original.Length; i++)
+            {
+                
+                if (String.IsNullOrWhiteSpace(original[i].ToString()))
+                {
+                    nexPos--;
+                }
+
+                if (nexPos == 0)
+                {
+                    newString += original[i].ToString().ToUpper();
+                }
+                else if (nexPos % 2 == 0)
+                {
+                    newString += original[i].ToString().ToUpper();
+                }
+                else
+                {
+                    newString += original[i].ToString().ToLower();
+                }
+                nexPos++;
+            }
+            return newString;
         }
     }
 }
